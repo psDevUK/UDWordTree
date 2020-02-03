46a798b88f7c2e692ced510b1571758b21852422
Adam <adambacon1@hotmail.com>
2020-02-03 20:04:35 +0000

    Add files via upload

---
+++ Components/UDWordTree.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import Chart from "react-google-charts";
+
+class UDWordTree extends React.Component {
+  render() {
+    const options = {
+      wordtree: {
+        format: "implicit",
+        word: this.props.word
+      }
+    }
+    return (
+      <div className="WordTree">
+        <Chart
+          chartType="WordTree"
+          width={this.props.width}
+          height={this.props.height}
+          data={this.props.data}
+          options={options}
+        />
+      </div>
+    )
+
+  }
+}
+
+export default UDWordTree
---
+++ Components/index.js
@@ -0,0 +1,2 @@
+import UDWordTree from './UDWordTree';
+UniversalDashboard.register("UD-WordTree", UDWordTree);
